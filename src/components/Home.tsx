39be1eeabd2992f17610b2ab20d4e0eb01b4a122
Asaph Velazquez <saulvelpa120@gmail.com>
2025-10-28 19:29:30 -0600

    initial commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import Secundario from "./secundario";
+
+function Home(){
+    return(
+        <div>
+            <Secundario/>
+            <h1>Welcome to the Home Page</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/secundario.tsx
@@ -0,0 +1,9 @@
+function Secundario(){
+    return(
+        <div>
+            <h1>este es un componente secundario</h1>
+        </div>
+    )
+}
+
+export default Secundario;
